c8d339005a2053a49415f788136a6d2abdc30584
ryz99 <rayaan.raza@tutanota.com>
2024-09-23 21:30:07 +0100

    added admin account, created 'to-do' items from admin view, added templates folder with base template

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Create database structure
+class Task(models.Model): #database table 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) #set many-to-one relationship using ForeignKey
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # Set string representation of model and it's official name
+    def __str__(self):
+        return self.title
+
+    # Order query set, order by complete status
+    class Meta:
+        ordering = ['complete']
