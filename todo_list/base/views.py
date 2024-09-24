38f42afadb92c7b463d692cbba7113e34e34a27f
ryz99 <rayaan.raza@tutanota.com>
2024-09-24 21:57:26 +0100

    added link from basic view to detail view, added task.html page which acts as DetailView page

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,7 +1,15 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
+# Looks for _list
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+# Looks for _detail
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
