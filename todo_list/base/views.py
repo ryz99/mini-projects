a0b84c6c5f48aa3089452337cf0698b3b41121b4
ryz99 <rayaan.raza@tutanota.com>
2024-09-24 22:16:49 +0100

    added task creation form link, task-creator

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
@@ -13,3 +16,9 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+#
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task')
