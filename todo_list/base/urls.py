1635cfe9a47383475b25e1a462b08c9d256beaf8
ryz99 <rayaan.raza@tutanota.com>
2024-09-26 19:06:34 +0100

    added logout, authentication needed to view/edit/delete tasks

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginViews
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginViews.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-creator/', TaskCreate.as_view(), name='task-creator'),
