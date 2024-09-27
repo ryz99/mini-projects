d895cd563ac443fc6d646ca254149c5a360b2a58
ryz99 <rayaan.raza@tutanota.com>
2024-09-27 21:10:20 +0100

    added registration form, logged in user no longer have access to login/registration pages, added search functionality

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -24,7 +25,16 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
-
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+        
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 # Create your views here.
 # Looks for _list
@@ -36,6 +46,12 @@ class TaskList(LoginRequiredMixin, ListView):
         context =  super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('searchbar') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+
+        context['search_input'] = search_input
         return context
 
 # Looks for _detail (overwrote with task.html)
